46e573f1fb1f679edf500bbc1e3b8c538e129393
Mahinder Kumar <mahinderkumar710@gmail.com>
2023-08-27 16:35:31 +0530

    Hello Elezabeth

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,29 @@
 // Put your application javascript here
+
+// mobile nav
+
+let mobileButton = document.querySelector(".mobile-menu-btn");
+let mobileNavbar = document.querySelector(".mobile-menu-wrap");
+let closeButton = document.querySelector(".close-menu"); 
+
+let submenuButton = document.querySelectorAll(".mobile-submenu-btn");
+
+
+
+mobileButton.addEventListener("click", function(){
+    mobileNavbar.classList.remove("hidden");
+})
+closeButton.addEventListener("click", function(){
+    mobileNavbar.classList.add("hidden");
+})
+
+
+submenuButton.forEach(toggle =>{
+    toggle.addEventListener("click", function(e){
+        e.preventDefault();
+        const submenu = this.parentNode.querySelector('.submenu-items');
+        console.log(submenu, 'submenu')
+        submenu.classList.toggle('submenu-open');
+    })
+})
+
